9bec72034014eef53bc686ddf0dc3275fd8b3366
saurabh-singh-9090 <90360saurabh@gmail.com>
2025-04-02 14:17:19 +0530

    contribute section ui updates

--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -190,7 +190,7 @@ export default function ContributePage() {
       </div>
       
       {activeTab === 'write' ? (
-        <Card>
+        <Card className='py-6'>
           <CardHeader>
             <CardTitle>Write a New Post</CardTitle>
             <CardDescription>
